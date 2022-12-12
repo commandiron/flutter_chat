b740dbb44a34f1ee126b64a9f782fb7bdcb5d481
Emir Demirli <commandiron@gmail.com>
2022-12-12 10:12:16 +0300

    .

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) => Container(
+                padding: EdgeInsets.all(8),
+                child: Text("This works."),
+              )),
+    );
+  }
+}
